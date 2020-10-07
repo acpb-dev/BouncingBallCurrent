6814e349b33d2ae501d61182d5afc863a52c0028
Bergy <akaBergy@gmail.com>
2020-10-07 16:31:14 -0400

    Initial commit

---
+++ src/cegepst/Main.java
@@ -0,0 +1,12 @@
+package cegepst;
+
+import cegepst.engine.GameWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameWindow window = new GameWindow();
+        window.start();
+
+    }
+}
